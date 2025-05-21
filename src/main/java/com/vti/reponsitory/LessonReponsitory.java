cd2b94c680212149d015b970a823b7189b946bf1
tuanminh203 <tuanminhle1210@gmail.com>
2025-05-21 13:02:28 +0700

    Initial commit

--- src/main/java/com/vti/reponsitory/LessonReponsitory.java
+++ src/main/java/com/vti/reponsitory/LessonReponsitory.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LessonReponsitory extends JpaRepository<Lesson, Integer> {
-    Lesson findByLessonNameAndLessonId(String lessonName, Integer lessonId);
 }
